9d69177fa7ab20995f275aaf52ad9a989c44e10b
Razinkov Konstantin <Korazinkov@gmail.com>
2019-04-01 14:37:03 +0300

    first commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,28 @@
+class TodosController < ApplicationController
+
+
+
+def index
+@todos = Todo.all
+@todo = Todo.new
+end
+
+def show
+ @todo = Todo.find(params[:id])
+end
+
+def new
+  @todo = Todo.new
+end
+
+def create
+ @todo = Todo.create todo_params
+redirect_to "#"
+end
+
+private
+    def todo_params
+      params.require(:todo).permit(:title, :description)
+    end
+
+end
